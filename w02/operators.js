567788ad4c65b440a8836b637094c0cf93cd0b33
natelandrum <nate178dawg@gmail.com>
2024-01-11 14:02:26 -0600

    Add w02-task

---
+++ w02/operators.js
@@ -0,0 +1,13 @@
+console.log(`Add 5 + 2 : ${5 + 2}`)
+console.log(`Multiply 5 * 2 : ${5 * 2}`)
+console.log(`Subtract 5 - 2 : ${5 - 2}`)
+console.log(`Concatinate bat + man : ${"bat" + "man"}`)
+let x = 2;
+console.log(`x = 2 : ${x}`) 
+x += 3
+console.log(`x + 3 : ${x}`)
+let y = 5;
+console.log(`x = y : ${x == y}`)
+console.log(`x < y : ${x < y}`)
+console.log(`x != y : ${x !== y}`)
+console.log(`x < 10 and x > 0 : ${x < 10 && x > 0}`)
